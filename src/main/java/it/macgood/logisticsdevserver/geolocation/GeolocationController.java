681c6a6436fc9fbc9faf648e2497dfdd0200afe8
macgood <bo2002277@gmail.com>
2023-10-13 01:55:19 +0300

    version 4.1

--- src/main/java/it/macgood/logisticsdevserver/geolocation/GeolocationController.java
+++ src/main/java/it/macgood/logisticsdevserver/geolocation/GeolocationController.java
@@ -5,6 +5,7 @@ import it.macgood.logisticsdevserver.carriage.model.ResponseCarriage;
 import it.macgood.logisticsdevserver.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,11 +18,24 @@ public class GeolocationController {
 
     @GetMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
     public GeolocationResponse getGeolocationInfoForDriver(
-            @RequestParam(required = false) String driverId,
+            @RequestParam String driverId,
             @RequestBody Geolocation current
     ) {
         ResponseCarriage carriage = carriageService.findByDriver(Long.parseLong(driverId));
         return geolocationService.getInformation(carriage, current);
+    }
+
+    @GetMapping(value = "accept", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> accept(
+            @RequestParam String driverId
+    ) {
+        try {
+            ResponseCarriage byDriver = carriageService.findByDriver(Long.parseLong(driverId));
+            carriageService.changeStatus(byDriver.getId());
+            return ResponseEntity.ok(true);
+        } catch (Exception e) {
+            return ResponseEntity.ok(false);
+        }
 
     }
 }
